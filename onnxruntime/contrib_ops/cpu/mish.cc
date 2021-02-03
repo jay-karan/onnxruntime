ebf5c088cdd633918dd684a4ca6d27e461873630
jay-karan <65300616+jay-karan@users.noreply.github.com>
2021-02-03 11:23:34 +0530

    Created mish.cc

---
+++ onnxruntime/contrib_ops/cpu/mish.cc
@@ -0,0 +1,14 @@
+#include "contrib_ops/cpu/mish.h"
+namespace onnxruntime {
+namespace contrib {
+
+ONNX_OPERATOR_KERNEL_EX(
+    Mish,
+    kOnnxDomain,
+    1,
+    kCpuExecutionProvider,
+    KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
+    Mish<float>
+);
+
+}
