6c786055555de9ac2247bde3df580ca318ad631d
jay-karan <65300616+jay-karan@users.noreply.github.com>
2021-02-05 13:23:57 +0530

    Update mish.cc

--- onnxruntime/contrib_ops/cpu/mish.cc
+++ onnxruntime/contrib_ops/cpu/mish.cc
@@ -2,14 +2,21 @@
 namespace onnxruntime {
 namespace contrib {
 
-ONNX_OPERATOR_KERNEL_EX(
-    Mish,
-    kOnnxDomain,
-    1,
-    kCpuExecutionProvider,
-    KernelDefBuilder().TypeConstraint("T", BuildKernelDefConstraints<float, double>()),
-    Mish
-);
+#define REGISTER_KERNEL_TYPED(T)                                  \
+  ONNX_OPERATOR_TYPED_KERNEL_EX(                                  \
+      Mish,                                     \
+      kOnnxDomain,                                                  \
+      1,                                                          \
+      T,                                                          \
+      kCpuExecutionProvider,                                      \
+      KernelDefBuilder()                                          \
+          .TypeConstraint("T", DataTypeImpl::GetTensorType<T>()), \
+      Mish<T>);
+
+REGISTER_KERNEL_TYPED(float)
+REGISTER_KERNEL_TYPED(double)
 
 }
 }
+
+
